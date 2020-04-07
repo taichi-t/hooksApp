bcb7bf7cb0f27c3ca928c3981304b7f6bac1e3a3
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-04-07 12:00:00 +0900

    first commit

---
+++ src/components/SongList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function SongList() {
+  return (
+    <div className="song-list">
+      <ul>
+        <li>this wild darkness</li>
+        <li>memory gospel</li>
+      </ul>
+    </div>
+  );
+}
