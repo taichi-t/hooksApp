3b2bcfdb1f79e2fad50628fd18917183b30a15ca
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-04-08 12:00:00 +0900

    created: SongList Compoent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import SongList from "./components/SongList";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <SongList />
+    </div>
+  );
 }
 
 export default App;
--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SongList() {
+  const [songs, setSongs] = useState([
+    { title: "almost home", id: 1 },
+    { title: "memory gospel", id: 2 },
+    { title: "this wild darkness", id: 3 },
+  ]);
+  const addSong = () => {
+    setSongs([...songs, { title: "new song", id: 4 }]);
+  };
   return (
     <div className="song-list">
       <ul>
-        <li>this wild darkness</li>
-        <li>memory gospel</li>
+        {songs.map((song) => {
+          return <li key={song.id}>{song.title}</li>;
+        })}
       </ul>
+      <button onClick={addSong}>Add a song</button>
     </div>
   );
 }
